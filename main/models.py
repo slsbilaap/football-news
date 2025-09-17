e3de83c3e68237d231fdce52c4eeb6e365c53a16
slsbilaap <aulyasalsabilaputri1@gmail.com>
2025-09-17 13:41:59 +0700

    Tutorial 3 done

--- main/models.py
+++ main/models.py
@@ -1,5 +1,6 @@
 import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
 class News(models.Model):
     CATEGORY_CHOICES = [
@@ -20,6 +21,8 @@ class News(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     is_featured = models.BooleanField(default=False)
     
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True) #Tutorial 3
+
     def __str__(self):
         return self.title
     
